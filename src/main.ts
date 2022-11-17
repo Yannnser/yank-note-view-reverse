af62c7cf84e7d53e475829bda0cb96f92752676d
Yanser <2306974571@qq.com>
2022-11-17 18:11:27 +0800

    优化代码

--- src/main.ts
+++ src/main.ts
@@ -4,37 +4,36 @@ const extensionName = __EXTENSION_ID__
 
 registerPlugin({
   name: extensionName,
-  register: (ctx) => {
-    if (localStorage.getItem('ysIsReverse') == null) {
-      localStorage.setItem('ysIsReverse', String(0))
-    }
-    let ysIsReverse = Number(localStorage.getItem('ysIsReverse'))
-    const toReverseCode = '.content{flex-direction:row-reverse}.sash-left{left:auto !important;right:0 !important}div.content > div.preview{transform:translateX(0)}.markdown-view .scroll-to-top{bottom:40px !important}.markdown-view .markdown-body .custom-container.group .group-item-radio{display:none}'
-    const recoveredCode = '.content{flex-direction:initial}.sash-left{left:initial !important;right:initial !important}div.content > div.preview{transform:initial}.markdown-view .scroll-to-top{bottom:initial !important}.markdown-view .markdown-body .custom-container.group .group-item-radio{display:initial}'
+  register: async (ctx) => {
+    let ysViewReverse = Boolean(ctx.storage.get('ysViewReverse', false))
 
-    function loadCssCode (code) {
-      const style = document.createElement('style')
-      style.type = 'text/css'
-      style.rel = 'stylesheet'
-      style.appendChild(document.createTextNode(code))
-      const head = document.getElementsByTagName('head')[0]
-      head.appendChild(style)
+    const toReverseCode =
+      '.content{flex-direction:row-reverse}.sash-left{left:auto !important;right:0 !important}div.content > div.preview{transform:translateX(0)}.markdown-view .scroll-to-top{bottom:40px !important}.markdown-view .markdown-body .custom-container.group .group-item-radio{display:none}'
+
+    let style
+    async function addStyle () {
+      if (!ysViewReverse) {
+        style.remove()
+        return
+      }
+      style = await ctx.theme.addStyles(toReverseCode)
     }
 
-    loadCssCode(ysIsReverse ? toReverseCode : recoveredCode)
+    addStyle()
 
     ctx.statusBar.tapMenus((menus) => {
-      menus['plugin-view-reverse'] = {
-        id: 'plugin-view-reverse',
-        position: 'left',
-        tips: '预览和编辑翻转',
-        title: '翻转',
+      menus['status-bar-view'].list?.push({
+        id: 'toggle-view-reverse',
+        type: 'normal',
+        checked: ysViewReverse,
+        title: '编辑器/预览翻转',
         onClick: () => {
-          ysIsReverse = Number(!ysIsReverse)
-          localStorage.setItem('ysIsReverse', String(ysIsReverse))
-          loadCssCode(ysIsReverse ? toReverseCode : recoveredCode)
+          ysViewReverse = !ysViewReverse
+          ctx.storage.set('ysViewReverse', ysViewReverse)
+          ctx.statusBar.refreshMenu()
+          addStyle()
         },
-      }
+      })
     })
   },
 })
